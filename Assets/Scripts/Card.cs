707c39d1ad2ce512ca70095aab17ccbe7fee01a2
kelvin3127 <kelvin3127@users.noreply.github.com>
2020-09-22 17:38:56 -0400

    Card Database
    Added a card database and a few basic cards

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Cards", order = 21)]
+
+public class Card : ScriptableObject
+{
+    public new string name;
+    public string description;
+    public string cardType;
+
+    public Sprite artwork;
+
+    public int playCost;
+    public int attackDamage;
+    public int blockAmount;
+    public int healAmount;
+
+}
